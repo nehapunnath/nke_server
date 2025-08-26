aa67c2b1457b3eb38690e262f216099a0c49b8ac
neha <nehapunnath@gmail.com>
2025-08-26 19:38:02 +0530

    'fifth'

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,18 @@
-// firebaseAdmin.js
+// // firebaseAdmin.js
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./config/serviceAccountkKey.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+// });
+
+// console.log('Firebase Admin SDK initialized successfully');
+// module.exports = admin;
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/serviceAccountkKey.json');
+
+// Parse service account key from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
