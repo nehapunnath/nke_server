b92a1fd9fc8137a81fdc8229f3885b187b5373ba
neha <nehapunnath@gmail.com>
2025-08-27 18:00:22 +0530

    'seven'

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,9 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const { getDatabase, ServerValue } = require('firebase-admin/database');
-const serviceAccount = require('./config/serviceAccountkKey.json');
+// const serviceAccount = require('./config/serviceAccountkKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
 
 const app = initializeApp({
   credential: cert(serviceAccount),
